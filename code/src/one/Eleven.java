c0ff4b453058a52c8cd37d21ae6f05d710305edd
xusu <ekkoxusu@163.com>
2018-04-19 19:24:49 +0800

    交替位二进制数 一些好用的工具类 强烈推荐 hutool

--- code/src/one/Eleven.java
+++ code/src/one/Eleven.java
@@ -1,13 +1,13 @@
-package test;
+package one;
 
-public class TestMain {
+public class Eleven {
 
     public static void main(String[] args) {
         System.out.println(new Solution().hasAlternatingBits(5));
     }
     static class Solution {
         //1.转化为二进制字符串 如果触雷则false
-        //2.使用二进制算法
+        //2.使用二进制算法 见答案-答案才是正解
         public boolean hasAlternatingBits(int n) {
             //1 10 101 1010
             //111  10 01 10 01|10 11
